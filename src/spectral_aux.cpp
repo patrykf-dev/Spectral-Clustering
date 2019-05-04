cd1ceaa4c525f54c661378e35e35a7f1e749143c
Patryk <patryk.gsrmeen@gmail.com>
2019-05-04 16:45:16 +0200

    Refactor cpp code

--- src/spectral_aux.cpp
+++ src/spectral_aux.cpp
@@ -1,43 +1,44 @@
 #include <cmath>
-#include<queue>
-#include<utility>
+#include <queue>
+#include <utility>
 #include <Rcpp.h>
 using namespace Rcpp;
 using namespace std;
 
+
+////////////////////////////////////////////////////////
+// 4.1 M-nearest neighbours ////////////////////////////
+////////////////////////////////////////////////////////
+
 class CompareDist
 {
 public:
-    bool operator()(pair<int,double> n1,pair<int,double> n2) {
+    bool operator()(pair<int,double> n1, pair<int,double> n2) {
         return n1.second > n2.second;
     }
 };
 
-double euclideanDistance(NumericVector point1, NumericVector point2);
-IntegerVector getNearestNeighbours(NumericMatrix points, int k, int pointIndex);
+double EuclideanDistance(NumericVector point1, NumericVector point2);
+IntegerVector GetNearestNeighbours(NumericMatrix points, int k, int pointIndex);
 
 
 // [[Rcpp::export]]
-IntegerMatrix mNearestNeighbours(NumericMatrix points, int k)
+IntegerMatrix Mnn(NumericMatrix points, int k)
 {
 	int n = points.nrow();
-
 	IntegerMatrix rc(n, k);
 
     for(int i = 0; i < n; i++)
     {
-    	IntegerVector result = getNearestNeighbours(points, k, i);
+    	IntegerVector result = GetNearestNeighbours(points, k, i);
         //Rcout << "Value for " << i << " is " << result(0) << " " << result(1) << " " << result(2) << std::endl;
-        
     	rc.row(i) = result;
     }
-
-
 	return rc;
 }
 
 
-IntegerVector getNearestNeighbours(NumericMatrix points, int k, int pointIndex)
+IntegerVector GetNearestNeighbours(NumericMatrix points, int k, int pointIndex)
 {
 	int n = points.nrow();
 	IntegerVector rc(k);
@@ -49,7 +50,7 @@ IntegerVector getNearestNeighbours(NumericMatrix points, int k, int pointIndex)
 		if(i == pointIndex)
 			continue;
 
-		double distance = euclideanDistance(points.row(i), points.row(pointIndex));
+		double distance = EuclideanDistance(points.row(i), points.row(pointIndex));
 		queue.push(make_pair(i, distance));
 	}
 
@@ -58,13 +59,11 @@ IntegerVector getNearestNeighbours(NumericMatrix points, int k, int pointIndex)
 		rc(i) = queue.top().first;
 		queue.pop();
 	}
-
-
 	return rc;
 }
 
 
-double euclideanDistance(NumericVector point1, NumericVector point2)
+double EuclideanDistance(NumericVector point1, NumericVector point2)
 {
 	int d = point1.size();
 	double result = 0;
@@ -72,3 +71,12 @@ double euclideanDistance(NumericVector point1, NumericVector point2)
 		result += (point1(i) - point2(i)) * (point1(i) - point2(i));
 	return sqrt(result);
 }
+
+
+
+////////////////////////////////////////////////////////
+// 4.2 Adjacency matrix ////////////////////////////////
+////////////////////////////////////////////////////////
+
+// TODO...
+
