9490d98db1c82a55cbe113c8e194b8c10b78c171
Patryk <patryk.gsrmeen@gmail.com>
2019-05-07 19:31:13 +0200

    Add testing functions

--- src/spectral.R
+++ src/spectral.R
@@ -1,5 +1,24 @@
 setwd("D:/Studia/_PrzetwarzanieDanych/praca_domowa2/Spectral-Clustering/src")
 Rcpp::sourceCpp("spectral_aux.cpp", verbose=TRUE, rebuild=TRUE)
+library(ggplot2)
+
+
+Laplacian_eigen <- function(D, G, k) {
+    L <- D - G;
+    E <- eigen(L, symmetric=TRUE)$vectors;
+    return(E[,(k+1):2]);
+}
+
+Kmeans_division <- function(X, k) {
+    #https://stackoverflow.com/questions/28273012/kmeans-gives-wrong-cluster-in-r
+    Xscaled = X;
+    Xscaled[,1] = scale(Xscaled[,1]);
+    Xscaled[,2] = scale(Xscaled[,2]);
+    clusterResult <- kmeans(Xscaled, k);
+    plot(Xscaled, col=clusterResult$cluster);
+    text(Xscaled, labels=clusterResult$cluster, cex=0.7, pos=2);
+}
+
 
 # example 1
 X1 <- matrix(c(0, 10, 1, 10, 3, 10, 6, 10, 14, 2, 16, 2, 19, 2), ncol=2, byrow=TRUE)
@@ -62,6 +81,12 @@ G2_expected <- matrix(c(
 stopifnot(all(G2 == G2_expected))
 
 L2 <- D2 - G2
-E2 <- eigen(L2)$vectors
+E2 <- Laplacian_eigen(D2, G2, 4)
+result2 <- kmeans(E2, 4)
 
+
+
+distance <- function(x1, x2, y1, y2) {
+    return (sqrt((x1 - x2)**2 + (y1 - y2)**2));
+}
 
