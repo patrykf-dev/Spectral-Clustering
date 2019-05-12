7520309401c14a22808e016997f77c5bdc12df8e
Patryk <patryk.gsrmeen@gmail.com>
2019-05-12 16:01:11 +0200

    Work in progress...

--- src/benchmark_tests.R
+++ src/benchmark_tests.R
@@ -1,49 +1,3 @@
-setwd("D:/Studia/_PrzetwarzanieDanych/praca_domowa2/Spectral-Clustering/src")
-Rcpp::sourceCpp("spectral_aux.cpp", verbose=TRUE, rebuild=TRUE)
-X1 <- matrix(c(0, 10, 1, 10, 3, 10, 6, 10, 14, 2, 16, 2, 19, 2), ncol=2, byrow=TRUE)
-X2 <- matrix(c(1, 1, 3, 1, 6, 10, 10, 10, 12, 10, 19, 1, 21, 1, 24, 6), ncol=2, byrow=TRUE)
-library(dendextend)
-library(mclust)
-library(ggplot2)
-
-Laplacian_eigen <- function(D, G, k) {
-    L <- D - G;
-    E <- eigen(L, symmetric=TRUE)$vectors;
-    return(E[,(k+1):2]);
-}
-
-Spectral_clustering_raw <- function(X, k, M) {
-    S <- Mnn(X, M);
-    G <- Mnn_graph(S);
-    D <- Mnn_graph_D_matrix(G);
-    G <- Mnn_connect_graph(G);
-    E <- Laplacian_eigen(D, G, k);
-    set.seed(100);
-    return(kmeans(E, k));
-}
-
-
-Spectral_clustering_plot<- function(X, k, M) {
-    result <- Spectral_clustering_raw(as.matrix(X), k, M);
-    colors <- factor(result$cluster);
-    if (ncol(X) == 2) {
-        ggplot(X, aes(X[[1]], X[[2]])) + geom_point(aes(col=colors))+ 
-            scale_fill_brewer(palette = "Set1");
-    } else {
-        ggplot(X, aes(X[[1]], X[[2]], X[[3]])) + geom_point(aes(col=colors))+ 
-            scale_fill_brewer(palette = "Set1");
-    }
-}
-
-
-Spectral_clustering <- function(data, k, M, plot){
-    if(plot) {
-        Spectral_clustering_plot(data, k, M);
-    } else {
-        result <- Spectral_clustering_raw(as.matrix(data), k, M);
-        return(result$cluster);
-    }
-}
 
 Compare_methods <- function(fileName) {
     k <- 3;
@@ -63,7 +17,7 @@ Compare_methods <- function(fileName) {
     # Find similarity indices
     dt <- data.frame("Method" = "Custom M=4", "Index" = "FM", "Value" = FM_index(myResult4, labels)[1], stringsAsFactors = FALSE);
     dt[nrow(dt) + 1,] = list("Custom M=4", "AR", adjustedRandIndex(myResult4, labels));
-
+    
     dt[nrow(dt) + 1,] = list("Custom M=20", "FM", FM_index(myResult20, labels)[1]);
     dt[nrow(dt) + 1,] = list("Custom M=20", "AR", adjustedRandIndex(myResult20, labels));
     
