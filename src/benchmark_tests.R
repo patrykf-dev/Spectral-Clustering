3b8c10912a304a73b1017773fa38a0be9f26cb64
Patryk <patryk.gsrmeen@gmail.com>
2019-05-13 00:15:34 +0200

    Add kernlab part

--- src/benchmark_tests.R
+++ src/benchmark_tests.R
@@ -1,9 +1,91 @@
 source("spectral.R")
-Compare_methods <- function(fileName) {
-    k <- 3;
+
+Compare_hclust_methods <- function(fileName) {
     setwd(paste0("D:/Studia/_PrzetwarzanieDanych/praca_domowa2/zbiory-benchmarkowe"));
     data <- read.table(paste0(fileName, ".data.gz"));
     labels <- as.integer(read.table(paste0(fileName, ".labels0.gz"))[,1]);
+    k <- max(labels);
+    
+    # Process algorithms
+    hCompleteResult <- cutree(hclust(dist(data), method = "complete"), k);
+    hSingleResult <- cutree(hclust(dist(data), method = "single"), k);
+    hAverageResult <- cutree(hclust(dist(data), method = "average"), k);
+    hWardResult <- cutree(hclust(dist(data), method = "ward.D2"), k);
+    hMcquittyResult <- cutree(hclust(dist(data), method = "mcquitty"), k);
+    hMedianResult <- cutree(hclust(dist(data), method = "median"), k);
+    hCentroidResult <- cutree(hclust(dist(data), method = "centroid"), k);
+    
+    # Find similarity indices
+    dt <- data.frame("Method" = "hclust Complete", "Index" = "FM", "Value" = FM_index(hCompleteResult, labels)[1], stringsAsFactors = FALSE);
+    dt[nrow(dt) + 1,] = list("hclust Complete", "AR", adjustedRandIndex(hCompleteResult, labels));
+    
+    dt[nrow(dt) + 1,] = list("hclust Single", "FM", FM_index(hSingleResult, labels)[1]);
+    dt[nrow(dt) + 1,] = list("hclust Single", "AR", adjustedRandIndex(hSingleResult, labels));
+    
+    dt[nrow(dt) + 1,] = list("hclust Average", "FM", FM_index(hAverageResult, labels)[1]);
+    dt[nrow(dt) + 1,] = list("hclust Average", "AR", adjustedRandIndex(hAverageResult, labels));
+    
+    dt[nrow(dt) + 1,] = list("hclust ward.D2", "FM", FM_index(hWardResult, labels)[1]);
+    dt[nrow(dt) + 1,] = list("hclust ward.D2", "AR", adjustedRandIndex(hWardResult, labels));
+    
+    dt[nrow(dt) + 1,] = list("hclust mcquitty", "FM", FM_index(hMcquittyResult, labels)[1]);
+    dt[nrow(dt) + 1,] = list("hclust mcquitty", "AR", adjustedRandIndex(hMcquittyResult, labels));
+    
+    dt[nrow(dt) + 1,] = list("hclust median", "FM", FM_index(hMedianResult, labels)[1]);
+    dt[nrow(dt) + 1,] = list("hclust median", "AR", adjustedRandIndex(hMedianResult, labels));
+    
+    dt[nrow(dt) + 1,] = list("hclust centroid", "FM", FM_index(hCentroidResult, labels)[1]);
+    dt[nrow(dt) + 1,] = list("hclust centroid", "AR", adjustedRandIndex(hCentroidResult, labels));
+    
+    return(dt);
+}
+
+compared <- Compare_hclust_methods("wingnut")
+
+ggplot(compared, aes(factor(Method), Value, fill = Index)) + 
+    geom_bar(stat="identity", position = "dodge") + 
+    scale_fill_brewer(palette = "Set1") + 
+    geom_text(aes(label = round(Value, digits = 2)), position=position_dodge(width=0.9), vjust=-0.25, angle = 90, hjust = -0.1)
+
+
+Benchmark_kernlab_method <- function(fileName) {
+    setwd(paste0("D:/Studia/_PrzetwarzanieDanych/praca_domowa2/zbiory-benchmarkowe"));
+    data <- read.table(paste0(fileName, ".data.gz"));
+    labels <- as.integer(read.table(paste0(fileName, ".labels0.gz"))[,1]);
+    k <- max(labels)
+    
+    start_time <- Sys.time()
+    kernlabResult <- kernlab::specc(as.matrix(data), k);
+    end_time <- Sys.time()
+    print(paste("SPECC", (end_time - start_time)))
+    
+    start_time <- Sys.time()
+    unlistedVals <- unclass(kernlabResult)
+    dt <- data.frame("Set name" = fileName, "Index" = "FM", "Value" = FM_index(unlistedVals, labels)[1], stringsAsFactors = FALSE);
+    dt[nrow(dt) + 1,] = list(fileName, "AR", adjustedRandIndex(unlistedVals, labels));
+    end_time <- Sys.time()
+    print(paste("INDICES", (end_time - start_time)))
+    
+    return(dt)
+}
+
+benchmarkSets <- c("atom", "line", "hepta", "ring", "smile", "tetra", "z1", "x1")
+kernlabResult <- lapply(benchmarkSets, Benchmark_kernlab_method)
+ 
+
+ggplot(df, aes(factor(Set.name), Value, fill = Index)) + 
+    geom_bar(stat="identity", position = "dodge") + 
+    scale_fill_brewer(palette = "Set1") + 
+    geom_text(aes(label = round(Value, digits = 2)), position=position_dodge(width=0.9), vjust=-0.25) +
+    xlab("Zbiór") + ylab("Wartość wskaźnika")
+
+
+
+Compare_methods <- function(fileName) {
+    setwd(paste0("D:/Studia/_PrzetwarzanieDanych/praca_domowa2/zbiory-benchmarkowe"));
+    data <- read.table(paste0(fileName, ".data.gz"));
+    labels <- as.integer(read.table(paste0(fileName, ".labels0.gz"))[,1]);
+    k <- max(labels);
     
     # Process algorithms
     # TODO: add finding best k for custom algorithm
@@ -35,20 +117,3 @@ Compare_methods <- function(fileName) {
     
     return(dt);
 }
-
-compared <- Compare_methods("wingnut")
-
-ggplot(compared, aes(factor(Method), Value, fill = Index)) + 
-    geom_bar(stat="identity", position = "dodge") + 
-    scale_fill_brewer(palette = "Set1")
-
-
-
-# CHECKING SETS DIMENSIONS
-Test_set_dimension <- function(name) {
-    data <- as.matrix(read.table(paste0(name, ".data.gz")));
-    labels <- as.matrix(read.table(paste0(name, ".labels0.gz"))[,1]);
-    print(paste("Data ", name, " is ", ncol(data), "x", nrow(data), " and labels is ", ncol(labels), "x", nrow(labels)))
-}
-names <- c("atom", "chainlink", "engytime", "lsun", "twodiamonds", "wingnut")
-asd <- lapply(names, Test_set_dimension)
